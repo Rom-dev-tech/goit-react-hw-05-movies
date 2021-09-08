aefa67902c99b954c6c507bd054340caa7746fe4
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2021-09-08 16:04:47 +0300

    adds search params in MoviesPage

--- src/components/views/MoviesView/MoviesView.jsx
+++ src/components/views/MoviesView/MoviesView.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useHistory, useLocation } from 'react-router';
 import { SearchBar } from './SearchBar';
 import { MoviesGalerry } from '../../MoviesGalerry/MoviesGalerry';
 import { Title } from '../../Title/Title';
@@ -8,26 +9,44 @@ import '../MoviesView/MoviesView.scss';
 export const MoviesView = () => {
   const [query, setQuery] = useState('');
   const [movies, setMovies] = useState([]);
+  const history = useHistory();
+  const location = useLocation();
+
+  const prevQuery = new URLSearchParams(location.search).get('query') ?? '';
 
   const onChangeSubmit = (query) => {
     setQuery(query);
     setMovies([]);
+
+    history.push({
+      ...location,
+      search: `query=${query}`,
+    });
   };
 
   useEffect(() => {
+    if (prevQuery !== '') {
+      moviesShelfAPI
+        .fetchSearchMovies(prevQuery)
+        .then((response) => setMovies(response.results));
+    }
+
     if (query !== '') {
       moviesShelfAPI
         .fetchSearchMovies(query)
         .then((response) => setMovies(response.results));
     }
     return;
-  }, [query]);
+  }, [prevQuery, query]);
 
   return (
     <>
       <SearchBar onSubmit={onChangeSubmit} />
 
       {query !== '' && <Title title="Found on request:" query={query} />}
+      {query !== prevQuery && (
+        <Title title="Found on request:" query={prevQuery} />
+      )}
 
       <MoviesGalerry movies={movies} />
     </>
